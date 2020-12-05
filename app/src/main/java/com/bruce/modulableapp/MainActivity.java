a68305633118b529379736b6458dfd1d997feac3
zhelong111 <907160968@qq.com>
2020-12-05 15:12:57 +0800

    UI views and exceptions

--- app/src/main/java/com/bruce/modulableapp/MainActivity.java
+++ app/src/main/java/com/bruce/modulableapp/MainActivity.java
@@ -1,14 +1,10 @@
 package com.bruce.modulableapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
-
+import androidx.appcompat.app.AppCompatActivity;
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.bruce.moduleapp.chat.ChatUtil;
-
-import network.httpclient.NetUtil;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
